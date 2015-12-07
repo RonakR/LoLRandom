f41a6c4b518f5bcc6b05083f0b35007e23a907ac
Ron Raithatha <ronaksraithatha@gmail.com>
2015-12-07 10:08:22 -0500

    Added styles.css, selected champion and random champion functionality

--- public/javascript/core.js
+++ public/javascript/core.js
@@ -2,20 +2,26 @@ var lolRandom = angular.module('lolRandom', []);
 
 lolRandom.controller('mainController', ['$scope', '$http', 
 	function ($scope, $http){
-	$scope.testData = "testing";
+
+	$scope.champions = ""; //Response from API
+	$scope.champion = ""; //Selected Champ. Either by random or click.
 
 	$(".chosen-select").chosen();
-	$scope.testing = function(){
-		console.log("tesging");
-	};
+
 	$scope.generate = function(){
 		// var sendVar = {tags: $(".chosen-select").val()};
 		var sendVar = $(".chosen-select").val();
-		console.log(JSON.stringify(sendVar));
 		$http.post('api/championsByRoles', sendVar)
 			.success(function(data){
-				console.log(data);
 				$scope.champions = data;
 			});	
 	};
+	$scope.championSelected = function(champion){
+		$scope.champion = champion;	
+	}
+	$scope.randomize = function(){
+		var numChampions = $scope.champions.length;
+		var randomNum = Math.floor((Math.random()*numChampions)+1);
+		$scope.champion = $scope.champions[randomNum];
+	}
 }]);
