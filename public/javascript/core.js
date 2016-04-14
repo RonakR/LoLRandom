735f70cbd9ce939cb9968976e1f60fb40a0654e4
Ron Raithatha <ronaksraithatha@gmail.com>
2016-04-13 21:08:29 -0400

    added particle effect

--- public/javascript/core.js
+++ public/javascript/core.js
@@ -1,8 +1,10 @@
 var lolRandom = angular.module('lolRandom', []);
 
-lolRandom.controller('mainController', ['$scope', '$http', 
+lolRandom.controller('mainController', ['$scope', '$http',
 	function ($scope, $http){
-
+		window.particlesJS.load('particles-js', 'assets/particlejs-config.json', function() {
+		  console.log('callback - particles.js config loaded');
+		});
 	$scope.champions = ""; //Response from API
 	$scope.champion = ""; //Selected Champ. Either by random or click.
 
@@ -15,10 +17,10 @@ lolRandom.controller('mainController', ['$scope', '$http',
 		$http.post('api/championsByRoles', sendVar)
 			.success(function(data){
 				$scope.champions = data;
-			});	
+			});
 	};
 	$scope.championSelected = function(champion){
-		$scope.champion = champion;	
+		$scope.champion = champion;
 	}
 	$scope.randomize = function(){
 		var numChampions = $scope.champions.length;
