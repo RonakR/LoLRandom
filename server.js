68ad8d68ad20298ba230341454ba56d9170f70d1
Ron Raithatha <ronaksraithatha@gmail.com>
2017-01-03 22:21:24 -0500

    removed standard js and added eslint airbnb style

--- server.js
+++ server.js
@@ -1,116 +1,86 @@
-var express = require('express')
-var app = express()
-var request = require('request')
-var mongoose = require('mongoose')
-var _ = require('underscore')
-var fs = require('fs')
-var bodyParser = require('body-parser')
-var session = require('express-session')
-const path = require('path')
+const express = require('express');
+const request = require('request');
+const mongoose = require('mongoose');
+const _ = require('underscore');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
 // Keys should be in a config file
-var riotAPIKey = '83d42fea-41fb-4688-be17-7bf6b7b80c0d'
-var championGGKey = '4442f8cded7a71423321527a8391ef79'
+const riotAPIKey = '83d42fea-41fb-4688-be17-7bf6b7b80c0d';
+const championGGKey = '4442f8cded7a71423321527a8391ef79';
 
-mongoose.connect('mongodb://localhost/lolrandom')
+const app = express();
 
-app.use(express.static('./public'))
-app.use(bodyParser.urlencoded({'extended': 'true'}))            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json())                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })) // parse application/vnd.api+json as json
-app.use(session({cookie: {maxAge: 60000}, secret: 'api', resave: false, saveUninitialized: false}))
+mongoose.connect('mongodb://localhost/lolrandom');
+
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: 'true' }));           // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(session({ cookie: { maxAge: 60000 }, secret: 'api', resave: false, saveUninitialized: false }));
 
 // Champion Model in Mongo
-var Champion = mongoose.model('Champion', {
+const Champion = mongoose.model('Champion', {
   tags: Array,
   laneInfo: Array,
-  name: String
-})
+  name: String,
+});
 
-// Populates the db by hitting riot's api.
-app.get('/api/riotTags', function (req, res) {
-  // Version number in config
-  request('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=tags&api_key=' + riotAPIKey, function (err, response, body) {
-    if (err) return console.log(err)
-    var data = JSON.parse(body)
-    populateFromData(data.data)
-  })
-  res.send('success')
-})
-
-var populateFromData = function (data) {
-  // ONLY ADDS RIGHT NOW
-  // needs to update existing ones
-  _.each(data, function (element, index, list) {
-    var allTags = []
-    var getMoreChampionDataPromise = getMoreChampionData(index)
-    var addToMongoPromise
-    var getChampionImagePromise
-    getMoreChampionDataPromise.then(function (data) {
-      allTags = _.union(data.tags, element.tags)
-      addToMongoPromise = addToMongo(allTags, data.laneInfo, index)
-      getChampionImagePromise = getChampionImage(index)
-    })
-
-    Promise.all([addToMongoPromise, getChampionImagePromise]).catch(function (err) {
-      throw err
-    })
-  })
-  console.log('added/updated')
+function createLaneInfoObj(patchWin, patchPlay) {
+  return {
+    patchWin: patchWin[patchWin.length - 1] > patchWin[patchWin.length - 2] ? 'UP' : 'DOWN',
+    patchPlay: patchPlay[patchPlay.length - 1] > patchPlay[patchPlay.length - 2] ? 'UP' : 'DOWN',
+  };
 }
 
 // Populats more tags from champoin.gg's api
-var getMoreChampionData = function (championName) {
-  var retObj = {
+function getMoreChampionData(championName) {
+  const retObj = {
     tags: [],
-    laneInfo: []
-  }
+    laneInfo: [],
+  };
 
-  return new Promise(function (resolve, reject) {
-    request('http://api.champion.gg/champion/' + championName + '?api_key=' + championGGKey, function (err, response, body) {
+  return new Promise((resolve, reject) => {
+    request(`http://api.champion.gg/champion/${championName}?api_key=${championGGKey}`, (err, response, body) => {
       if (err) {
-        console.log(err)
-        reject(err)
+        console.log(err);
+        reject(err);
       }
-      var data = JSON.parse(body)
+      const data = JSON.parse(body);
 
-      _.each(data, function (element, index, list) {
+      _.each(data, (element) => {
         // dmgComposition
-        var damageType = element.dmgComposition.magicDmg > element.dmgComposition.physicalDmg ? 'Magic' : 'Physical'
+        const damageType = element.dmgComposition.magicDmg > element.dmgComposition.physicalDmg ? 'Magic' : 'Physical';
         if (!_.contains(retObj.tags, damageType)) {
-          retObj.tags.push(damageType)
+          retObj.tags.push(damageType);
         }
         // lanes
-        retObj.tags.push(element.role)
+        retObj.tags.push(element.role);
         // laneInfo
-        var laneInfoObj = createLaneInfoObj(element.patchWin, element.patchPlay)
-        var obj = {}
-        obj[element.role] = laneInfoObj
-        retObj.laneInfo.push(obj)
-      })
+        const laneInfoObj = createLaneInfoObj(element.patchWin, element.patchPlay);
+        const obj = {};
+        obj[element.role] = laneInfoObj;
+        retObj.laneInfo.push(obj);
+      });
 
-      resolve(retObj)
-    })
-  })
+      resolve(retObj);
+    });
+  });
 }
 
-var createLaneInfoObj = function (patchWin, patchPlay) {
-  return {
-    'patchWin': patchWin[patchWin.length - 1] > patchWin[patchWin.length - 2] ? 'UP' : 'DOWN',
-    'patchPlay': patchPlay[patchPlay.length - 1] > patchPlay[patchPlay.length - 2] ? 'UP' : 'DOWN'
-  }
-}
 
-var addToMongo = function (allTags, laneInformation, index) {
+function addToMongo(allTags, laneInformation, index) {
   return Champion.create({
     tags: allTags,
     laneInfo: laneInformation,
-    name: index
-  })
+    name: index,
+  });
 }
 
-var getChampionImage = function (championName) {
-  return new Promise(function (resolve, reject) {
-    var championNamePng = championName + '.png'
+function getChampionImage(championName) {
+  return new Promise((resolve, reject) => {
+    const championNamePng = `${championName}.png`;
     // Make patch number configurat
     request('http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' + championNamePng)
     .on('error', function (err) {
@@ -121,6 +91,39 @@ var getChampionImage = function (championName) {
     resolve('saved')
   })
 }
+
+function populateFromData(data) {
+  // ONLY ADDS RIGHT NOW
+  // needs to update existing ones
+  _.each(data, (element, index) => {
+    let allTags = [];
+    const getMoreChampionDataPromise = getMoreChampionData(index);
+    let addToMongoPromise;
+    let getChampionImagePromise;
+    getMoreChampionDataPromise.then((data) => {
+      allTags = _.union(data.tags, element.tags);
+      addToMongoPromise = addToMongo(allTags, data.laneInfo, index);
+      getChampionImagePromise = getChampionImage(index);
+    });
+
+    Promise.all([addToMongoPromise, getChampionImagePromise]).catch((err) => {
+      throw err;
+    });
+  });
+  console.log('added/updated');
+}
+
+// Populates the db by hitting riot's api.
+app.get('/api/riotTags', (req, res) => {
+  // Version number in config
+  const championDataUrl = `https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=tags&api_key=${riotAPIKey}`;
+  request(championDataUrl, (err, response, body) => {
+    if (err) throw err;
+    const data = JSON.parse(body);
+    populateFromData(data.data);
+  });
+  res.send('success');
+});
 
 app.post('/api/championsByRoles', function (req, res) {
   Champion.find(
