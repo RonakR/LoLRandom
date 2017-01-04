401c4c00a84dd0edbc25e0d0500e1f827c589825
Ron Raithatha <ronaksraithatha@gmail.com>
2017-01-03 21:18:47 -0500

    updated angular code to follow standard js

--- public/javascript/core.js
+++ public/javascript/core.js
@@ -1,39 +1,40 @@
-var lolRandom = angular.module('lolRandom', []);
+var lolRandom = angular.module('lolRandom', [])
 
 lolRandom.controller('mainController', ['$scope', '$http',
-	function ($scope, $http){
-		window.particlesJS.load('particles-js', 'assets/particlejs-config.json', function() {
-		  console.log('callback - particles.js config loaded');
-		});
-	$scope.champions = ""; //Response from API
-	$scope.champion = ""; //Selected Champ. Either by random or click.
+  function ($scope, $http) {
+    window.particlesJS.load('particles-js', 'assets/particlejs-config.json', function () {
+      console.log('callback - particles.js config loaded')
+    })
+    $scope.champions = '' // Response from API
+    $scope.champion = ''  // Selected Champ. Either by random or click.
 
-	$(".chosen-select").chosen();
+    $('.chosen-select').chosen()
 
-	$scope.generate = function(){
-		// var sendVar = {tags: $(".chosen-select").val()};
-		$scope.champion = "";
-		var sendVar = $(".chosen-select").val();
-		$http.post('api/championsByRoles', sendVar)
-			.success(function(data){
-				$scope.champions = data;
-				// $scope.alterData();
-			});
-		};
-	// $scope.alterData = function() {
-	// 	console.log($scope.champions)
-	// 	angular.forEach($scope.champions, function(value, key) {
-	// 		angular.forEach(value.laneInfo, function(laneValue, laneKey){
-	// 				console.log(laneValue);
-	// 		});
-	// 	});
-	// }
-	$scope.championSelected = function(champion){
-		$scope.champion = champion;
-	}
-	$scope.randomize = function(){
-		var numChampions = $scope.champions.length;
-		var randomNum = Math.floor((Math.random()*numChampions));
-		$scope.champion = $scope.champions[randomNum];
-	}
-}]);
+    $scope.generate = function () {
+      // var sendVar = {tags: $(".chosen-select").val()};
+      $scope.champion = ''
+      var sendVar = $('.chosen-select').val()
+      $http.post('api/championsByRoles', sendVar)
+      .success(function (data) {
+        $scope.champions = data
+        // $scope.alterData();
+      })
+    }
+    // $scope.alterData = function() {
+    //  console.log($scope.champions)
+    //  angular.forEach($scope.champions, function(value, key) {
+    //    angular.forEach(value.laneInfo, function(laneValue, laneKey){
+    //      console.log(laneValue);
+    //    });
+    //  });
+    // }
+    $scope.championSelected = function (champion) {
+      $scope.champion = champion
+    }
+    $scope.randomize = function () {
+      var numChampions = $scope.champions.length
+      var randomNum = Math.floor((Math.random() * numChampions))
+      $scope.champion = $scope.champions[randomNum]
+    }
+  }
+])
